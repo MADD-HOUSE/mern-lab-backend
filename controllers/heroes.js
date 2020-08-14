8dc1bb030bc5f85bf888f475172e877ae9509ed2
dylankgirard <dylankgirard@gmail.com>
2020-08-14 14:02:11 -0700

    Began routing heroes controller

--- controllers/heroes.js
+++ controllers/heroes.js
@@ -2,7 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const Hero = require('../models/Hero');
+
 router.get('/', (req, res) => {
 	Hero.find({}).then((hero) => res.json(hero));
 });
+
+router.get('/:id', (req, res) => {
+	Hero.find({ _id: req.params.id }).then((hero) => {
+		res.json(hero);
+	});
+});
+
+router.post('/', (req, res) => {
+	Hero.create(req.body).then((hero) => res.json(hero));
+});
+
+
+
 module.exports = router;
