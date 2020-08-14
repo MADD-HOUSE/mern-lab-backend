7eb2e0ffb3a36f7261a0eb0f9d7b28b794c0b86f
SpideyFanDan <dsmith.teach@gmail.com>
2020-08-14 13:27:28 -0600

    index.js initialized

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const heroesController = require('./controllers/heroes');
+app.use(express.json());
+app.get('/', (req, res) => {
+	res.redirect('/heroes');
+});
+app.use('/heroes', heroesController);
+app.listen(3000, () => console.log('app is running'));
