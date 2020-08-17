49a95d3fe42dbc3cdc5df21647f43da7495ae2f3
MorganMac1993 <sitesbymo@gmail.com>
2020-08-17 10:51:59 -0700

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const heroesController = require('./controllers/heroes');
 
+app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
 	res.redirect('/heroes');
